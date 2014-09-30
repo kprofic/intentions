69d9bae1f788d2afe91beee8cd1b44e53f17f31f
krzysztof <kprofic@gmail.com>
2014-09-30 21:14:15 +0200

    Fixed ViewModel observance

--- Intentions/PersonViewModel.m
+++ Intentions/PersonViewModel.m
@@ -11,6 +11,16 @@
 
 @implementation PersonViewModel
 
+- (instancetype)initWithModel:(id)model
+{
+    self = [super initWithModel:model];
+    if (self) {
+        [self.model addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew context:NULL];
+        [self.model addObserver:self forKeyPath:@"surname" options:NSKeyValueObservingOptionNew context:NULL];
+    }
+    return self;
+}
+
 - (Person *)personModel
 {
     return (Person *)self.model;
@@ -19,6 +29,17 @@
 - (NSString *)fullName
 {
     return [NSString stringWithFormat:@"%@ %@", self.personModel.name, self.personModel.surname];
+}
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (object == self.model && ([keyPath isEqualToString:@"name"] || [keyPath isEqualToString:@"surname"])) {
+        [self willChangeValueForKey:@"fullName"];
+        // we dont really update, its createn on demand
+        [self didChangeValueForKey:@"fullName"];
+    }
 }
 
 @end
